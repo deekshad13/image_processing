0b79b67e515f9fcadf2c105bea110561639742c6
deekshad13 <deeksha.d13703@gmail.com>
2026-03-12 16:50:04 +0530

    Add root redirect to Swagger docs

--- src/api/server.py
+++ src/api/server.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from src.api.routes import router
 
-app = FastAPI(title = "Rootstalk API")
+app = FastAPI(title="Rootstalk API")
 
 app.include_router(router)
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
